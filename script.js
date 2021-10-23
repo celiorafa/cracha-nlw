70e00533719a68fa01178913792857ad9c3e69f6
Celio Rafael Faria <74562171+celiorafa@users.noreply.github.com>
2021-10-23 11:46:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ function getGitHubProfileInfos() {
     })
 }
 getGitHubProfileInfos()
+
+var countDownDate = new Date('Dec 01, 2021 08:00:00').getTime()
+var x = setInterval(function () {
+  var now = new Date().getTime()
+  var distance = countDownDate - now
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+  document.getElementById('dowhile').innerHTML =
+    'FALTAM<BR>' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's '
+
+  if (distance < 0) {
+    clearInterval(x)
+    document.getElementById('dowhile').innerHTML = 'CHEGOU O DIA!'
+  }
+}, 1000)
